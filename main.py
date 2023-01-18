239bc30adb416c30820a593ae291b5201d0a284b
Przemyslaw Plata <paplata50@gmail.com>
2023-01-18 17:10:15 +0100

    Code clean-up and add requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from fpdf import FPDF, XPos, YPos
 
+# Creates dir for PDFs if it doesn't exist
 if not os.path.exists("./PDFs"):
     os.mkdir("./PDFs")
 
@@ -11,12 +12,14 @@ for i in os.listdir("./Invoices"):
     filepath = os.path.join("./Invoices", i)
 
     data = pd.read_excel(filepath)
+
     total_price = str(data["total_price"].sum())
 
     pdf.add_page()
 
     id_number, date = i[:-5].split("-", 1)
 
+    # Creating PDF layout
     pdf.set_font(family="Times", style="B", size=20)
     pdf.cell(
         w=0, h=12, txt=f"Invoice nr. {id_number}", new_x=XPos.LMARGIN, new_y=YPos.NEXT
@@ -29,6 +32,7 @@ for i in os.listdir("./Invoices"):
     line_height = pdf.font_size_pt
     col_widths = [23, 50, 39, 39, 39]
 
+    # Creating table
     for col, width in zip(data.columns, col_widths):
         formated_col = col.replace("_", " ")
         formated_col = formated_col.title()
@@ -46,16 +50,16 @@ for i in os.listdir("./Invoices"):
         pdf.ln(line_height)
         for value, width in zip(row.values, col_widths):
             pdf.multi_cell(
-            w=width,
-            h=line_height,
-            txt=str(value),
-            border=1,
-            new_x="RIGHT",
-            new_y="TOP",
-        )
+                w=width,
+                h=line_height,
+                txt=str(value),
+                border=1,
+                new_x="RIGHT",
+                new_y="TOP",
+            )
     pdf.ln(line_height)
-    for value, width in zip(["","","","",total_price], col_widths):
-            pdf.multi_cell(
+    for value, width in zip(["", "", "", "", total_price], col_widths):
+        pdf.multi_cell(
             w=width,
             h=line_height,
             txt=str(value),
@@ -64,10 +68,11 @@ for i in os.listdir("./Invoices"):
             new_y="TOP",
         )
 
-    pdf.ln(line_height*2)
-    
+    pdf.ln(line_height * 2)
+
     content = f"The total due amount is {total_price} Euros."
+    
     pdf.set_font(family="Times", style="B", size=14)
     pdf.cell(w=0, h=16, txt=content, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
-    
-    pdf.output(f"./PDFs/{os.path.basename(filepath)}.pdf")
+
+    pdf.output(f"./PDFs/{os.path.basename(filepath)[:-5]}.pdf")
